995b9076fa3bd2d51df9eb45575f451d45543d97
zyjwuyan <32290920+zyjwuyan@users.noreply.github.com>
2024-05-14 22:58:58 +0800

    Update i2d_head.py

--- mmaction/models/heads/i2d_head.py
+++ mmaction/models/heads/i2d_head.py
@@ -36,10 +36,12 @@ class I2DHead(BaseHead):
         self.spatial_type = spatial_type
         self.dropout_ratio = dropout_ratio
         self.init_std = init_std
+                     
         if self.dropout_ratio != 0:
             self.dropout = nn.Dropout(p=self.dropout_ratio)
         else:
             self.dropout = None
+            
         self.fc_cls = nn.Linear(self.in_channels, self.num_classes)
 
         if self.spatial_type == 'avg':
@@ -64,12 +66,16 @@ class I2DHead(BaseHead):
         # [N, in_channels, 4, 7, 7]
         if self.avg_pool is not None:
             x = self.avg_pool(x)
+            
         # [N, in_channels, 1, 1, 1]
         if self.dropout is not None:
             x = self.dropout(x)
+            
         # [N, in_channels, 1, 1, 1]
         x = x.view(x.shape[0], -1)
+        
         # [N, in_channels]
         cls_score = self.fc_cls(x)
+        
         # [N, num_classes]
         return cls_score
